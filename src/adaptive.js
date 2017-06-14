acbdc23b7a95f3a2e5b21ee3613772284286f0f6
ximolang <ximolang@yeah.net>
2017-06-15 01:05:54 +0800

    fix adaptive.js

--- src/adaptive.js
+++ src/adaptive.js
@@ -1,31 +1,11 @@
 (function (win) {
 
-  var dpr = 0, scale = 0, bodyFontSize = 12, adap = {}, doc = win.document
+  var dpr = 0, scale = 0, devicePixelRatio = 1, bodyFontSize = 12, timer = null, doc = win.document
   var htmlEle = doc.documentElement,
-      viewportEle = doc.querySelector('meta[name="viewport"]')
+    viewportEle = doc.querySelector('meta[name="viewport"]')
 
-  var refresh = function () {
-    var docWidth = htmlEle.clientWidth
-    if (docWidth / dpr > 540) {
-      docWidth = 540 * dpr
-    }
-
-    rootFontSize = docWidth / dpr
-
-    htmlEle.style.fontSize = rootFontSize + 'px'
-  }
-
-  // 如果已经存在viewport meta标签，则不动态改变
-  if (viewportEle) {
-    var matchArray = viewportEle.getAttribute('content').match(/initial-scale=([\d\.]+)/)
-    scale = matchArray[1]
-    dpr   = parseInt(1 / scale)
-  }
-
-  // 如果没有设置initial-scale则动态计算出dpr和scale
-  if (dpr === 0 && scale === 0) {
-    var devicePixelRatio = win.devicePixelRatio
-
+  var calcDprAndScale = function () {
+    devicePixelRatio = win.devicePixelRatio
     //判断各种手机的情况
     var iphone = win.navigator.appVersion.match(/iphone/gi)
     var android = win.navigator.appVersion.match(/android/gi)
@@ -38,22 +18,49 @@
       } else {
         dpr = 1
       }
-    } else if (android) {
-      dpr = 1
     } else {
-      console.warn('你用的是啥手机啊～_～...')
+      dpr = 1
     }
 
     // 设置缩放值
     scale = (1 / dpr).toFixed(6)
-
   }
 
-  htmlEle.setAttribute('data-dpr',dpr)
+  var refresh = function () {
+    var docWidth = htmlEle.clientWidth, rootFontSize
+
+    calcDprAndScale()
+
+    if (docWidth / dpr > 540) {
+      docWidth = 540 * dpr
+    }
+
+    htmlEle.setAttribute('data-dpr', dpr)
+
+    rootFontSize = docWidth / 10
+
+    htmlEle.style.fontSize = rootFontSize + 'px'
+    if (doc.body) doc.body.style.fontSize = bodyFontSize * dpr + 'px'
+  }
+
+  // 如果已经存在viewport meta标签，则不动态改变
+  if (viewportEle) {
+    var matchArray = viewportEle.getAttribute('content').match(/initial-scale=([\d\.]+)/)
+    scale = matchArray[1]
+    dpr = parseInt(1 / scale)
+  }
+
+  // 如果没有设置initial-scale则动态计算出dpr和scale
+  if (dpr === 0 && scale === 0) {
+    calcDprAndScale()
+  }
+
+  htmlEle.setAttribute('data-dpr', dpr)
+
   if (!viewportEle) {
     var meta = document.createElement('meta')
-    meta.setAttribute('name','viewport')
-    meta.setAttribute('content','initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no')
+    meta.setAttribute('name', 'viewport')
+    meta.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no')
     if (htmlEle.firstElementChild) {
       htmlEle.firstElementChild.appendChild(meta)
     } else {
@@ -62,13 +69,26 @@
   }
 
   // 文档加载成功设置body字体
-  if (win.document.readyState === 'complete') {
-    win.document.body.style.fontSize = bodyFontSize + 'px'
+  if (doc.readyState === 'complete') {
+    doc.body.style.fontSize = bodyFontSize * dpr + 'px'
   } else {
-    win.document.addEventListener('DOMContentLoaded', function(e) {
-      win.document.body.style.fontSize = bodyFontSize * dpr + 'px'
+    doc.addEventListener('DOMContentLoaded', function (e) {
+      doc.body.style.fontSize = bodyFontSize * dpr + 'px'
     }, false)
   }
 
+  refresh()
+
+  win.addEventListener('resize', function () {
+    clearTimeout(timer)
+    timer = setTimeout(refresh, 300)
+  }, false)
+
+  win.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      clearTimeout(timer);
+      timer = setTimeout(refresh, 300);
+    }
+  }, false);
 
 })(window)
